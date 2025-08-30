1e433d136099915b655eaf3fbc0a0e80d8944dbb
Doruat <mehmetdurmaz07@gmail.com>
2025-08-30 19:27:36 +0300

    Add files via upload

---
+++ SaferAtomicBombs_1.0.0/data.lua
@@ -0,0 +1,3 @@
+data.raw.projectile["atomic-bomb-ground-zero-projectile"].action[1].force="not-same"
+data.raw.projectile["atomic-bomb-wave"].action[1].force="not-same"
+data.raw.projectile["atomic-bomb-ground-zero-projectile"].action[1].force="not-same"
