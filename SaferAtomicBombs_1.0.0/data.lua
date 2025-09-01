cc534c64f0995711fc647435926872484a3df22c
Doruat <mehmetdurmaz07@gmail.com>
2025-09-01 20:54:06 +0300

    Reactors explode normally

--- SaferAtomicBombs_1.0.0/data.lua
+++ SaferAtomicBombs_1.0.0/data.lua
@@ -1,3 +1,14 @@
+local bombwave = table.deepcopy(data.raw.projectile["atomic-bomb-wave"])
+local groundzero = table.deepcopy(data.raw.projectile["atomic-bomb-ground-zero-projectile"])
+groundzero.name="groundzero"
+bombwave.name="bombwave"
+local rocket = table.deepcopy(data.raw.projectile["atomic-rocket"])
+rocket.name="rocket"
+data:extend({groundzero,bombwave})
+rocket.action.action_delivery.target_effects[12].action.action_delivery.projectile = "groundzero"
+rocket.action.action_delivery.target_effects[13].action.action_delivery.projectile = "bombwave"
+data:extend({rocket})
+data.raw["reactor"]["nuclear-reactor"].meltdown_action.action_delivery.target_effects[1].entity_name = "rocket"
+
 data.raw.projectile["atomic-bomb-ground-zero-projectile"].action[1].force="not-same"
 data.raw.projectile["atomic-bomb-wave"].action[1].force="not-same"
-data.raw.projectile["atomic-bomb-ground-zero-projectile"].action[1].force="not-same"
