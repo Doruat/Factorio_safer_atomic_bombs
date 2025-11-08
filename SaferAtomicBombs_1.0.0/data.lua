188353245d90465cb6169fe588bc28a23a87d75f
Doruat <mehmetdurmaz07@gmail.com>
2025-11-08 21:20:15 +0300

    Fixed normal rocket being atomic

--- SaferAtomicBombs_1.0.0/data.lua
+++ SaferAtomicBombs_1.0.0/data.lua
@@ -3,12 +3,13 @@ local groundzero = table.deepcopy(data.raw.projectile["atomic-bomb-ground-zero-p
 groundzero.name="groundzero"
 bombwave.name="bombwave"
 local rocket = table.deepcopy(data.raw.projectile["atomic-rocket"])
-rocket.name="rocket"
+rocket.name="atom-rocket"
 data:extend({groundzero,bombwave})
 rocket.action.action_delivery.target_effects[12].action.action_delivery.projectile = "groundzero"
 rocket.action.action_delivery.target_effects[13].action.action_delivery.projectile = "bombwave"
 data:extend({rocket})
-data.raw["reactor"]["nuclear-reactor"].meltdown_action.action_delivery.target_effects[1].entity_name = "rocket"
+data.raw["reactor"]["nuclear-reactor"].meltdown_action.action_delivery.target_effects[1].entity_name = "atom-rocket"
 
 data.raw.projectile["atomic-bomb-ground-zero-projectile"].action[1].force="not-same"
 data.raw.projectile["atomic-bomb-wave"].action[1].force="not-same"
+
